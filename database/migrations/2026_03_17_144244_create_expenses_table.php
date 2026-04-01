50a8590aa3599f283aa73ecf5a36183289559282
Umakant Yadav <37271073+UKY171991@users.noreply.github.com>
2026-04-01 12:11:38 +0530

    Update 2026_03_17_144244_create_expenses_table.php

--- database/migrations/2026_03_17_144244_create_expenses_table.php
+++ database/migrations/2026_03_17_144244_create_expenses_table.php
@@ -11,6 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
+        Schema::dropIfExists('expenses');
         Schema::create('expenses', function (Blueprint $table) {
             $table->id();
             $table->string('category'); // Travel, Events, Utilities, Staff, etc.
